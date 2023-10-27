af709af5dc7e2641aa94c13b116e384eb845550a
Md Azaz <mdazaz9026@gmail.com>
2023-10-27 22:13:39 +0530

    First node program in node.js

---
+++ test.js
@@ -0,0 +1,35 @@
+/**
+ * What is Node.js ?
+ * 
+ * node js  is widely used to build a backend Serivces like a APIs and talk to the DB.
+ * 
+ * Node js is an open Source , Corss Platfrom JavaScript , Runtime Enviroment  for executing Code outside of a Browser.
+ */
+
+/**
+ * Feature of Node.js ?
+ * 
+ * Single-Threaded
+ * Asynchronous
+ * Event Driven
+ * NPM
+ * Highly Scalable
+ * c++ and V8 Engine
+ */
+
+/**
+ * First node program in node.js
+ */
+console.log("hello Azaz")
+
+function sayHello(){
+    console.log('Hello From Azaz')
+}
+sayHello()
+
+/**
+ * it was Run by :- node test.js
+ * 
+ * hello Azaz
+ * Hello From Azaz
+ */
