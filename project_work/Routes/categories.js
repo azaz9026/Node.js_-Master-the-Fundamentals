370c51ccdead166163ae80969dbf5debddd88057
Md Azaz <mdazaz9026@gmail.com>
2023-11-04 22:47:46 +0530

    npm i joi

---
+++ project_work/Routes/categories.js
@@ -0,0 +1,80 @@
+const express = require('express')
+
+const joi = require('joi')
+
+const router = express.Router()
+
+const categories = [
+    {id:1 , name : 'Web'},
+    {id:2 , name : 'Mobile'},
+    {id:3 , name : 'Photography'},
+]
+
+
+/** Get Method */
+
+
+router.get('/api/categories', (req , res) => {
+    res.send(categories)
+})
+
+
+
+/** Post Method */
+
+
+router.post('/api/categories' , (req , res) => {
+ const category = {
+        id: categories.length + 1,
+        name : req.body.name
+  };
+    categories.push(category)
+    res.send(category)
+
+})
+
+
+/** Put Method */
+
+
+router.put('/api/categories/:id' , (req , res) => {
+
+    const category = categories.find(c => c.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send(' The Category with the given ID was not found. ')
+
+
+    category.name = req.body.name
+    res.send(category)
+
+})
+
+
+/** Delete Method */
+
+
+router.delete('/api/categories/:id' , (req , res) => {
+
+    const category = categories.find(c => c.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send(' The genre with the given ID was not found. ')
+
+
+    const index = categories.indexOf(category);
+    categories.splice(index , 1)
+    res.send(category)
+
+})
+
+
+/** Get Method with ID */
+
+
+router.get('/api/categories/:id' , (req , res) => {
+
+    const category = categories.find(c => c.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send(' The genre with the given ID was not found. ')
+
+    res.send(category)
+
+})
+
+module.exports = router
--- project_work/app.js
+++ project_work/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const categories = require('./Routes/categories')
 
 
 const app = express()
@@ -6,82 +7,11 @@ const app = express()
 /** Adding JSON Middleware */
 
 app.use(express.json())
-
-const categories = [
-    {id:1 , name : 'Web'},
-    {id:1 , name : 'Mobile'},
-    {id:1 , name : 'Photography'},
-]
-
-
-/** Get Method */
-
-
-app.get('/api/categories', (req , res) => {
-    res.send(categories)
-})
-
-
-
-/** Post Method */
-
-
-app.post('/api/categories' , (req , res) => {
- const category = {
-        id: categories.length + 1,
-        name : req.body.name
-  };
-    categories.push(category)
-    res.send(category)
-
-})
-
-
-/** Put Method */
-
-
-app.put('/api/categories/:id' , (req , res) => {
-
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if(!category) return res.status(404).send(' The Category with the given ID was not found. ')
-
-
-    category.name = req.body.name
-    res.send(category)
-
-})
-
-
-/** Delete Method */
-
-
-app.delete('/api/categories/:id' , (req , res) => {
-
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if(!category) return res.status(404).send(' The genre with the given ID was not found. ')
-
-
-    const index = categories.indexOf(category);
-    categories.splice(index , 1)
-    res.send(category)
-
-})
-
-
-/** Get Method with ID */
-
-
-app.get('/api/categories/:id' , (req , res) => {
-
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if(!category) return res.status(404).send(' The genre with the given ID was not found. ')
-
-    res.send(category)
-
-})
+app.use(categories)
 
 
 /** port number */
 
 const port = process.env.PORT || 3000
 app.listen(port , () => console.log( `the port is listening ${port}....` ))
+
