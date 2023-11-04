83db20b8241c86cc103940c74c4b43f0dea5a456
Md Azaz <mdazaz9026@gmail.com>
2023-11-04 21:53:45 +0530

    /** Delete Method */

--- project_work/app.js
+++ project_work/app.js
@@ -15,6 +15,45 @@ app.get('/api/categories', (req , res) => {
     res.send(categories)
 })
 
+
+/** Post Method */
+
 app.post('/api/categories' , (req , res) => {
+
+    const category = {
+        id : categories.length + 1,
+        name : req.body.name
+    }
+    categories.push(category)
+    res.send(category)
+
+})
+
+
+/** Put Method */
+
+app.put('/api/categories/:id' , (req , res) => {
+
+    const category = categories.find(c => c.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send(' The Category with the given ID was not found. ')
+
+    if(error) return res.status(404).send(error.details[0].message);
+
+    category.name = req.body.name
+    res.send(category)
+
+})
+
+/** Delete Method */
+
+app.delete('/api/categories/:id' , (req , res) => {
+
+    const category = categories.find(c => c.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send(' The genre with the given ID was not found. ')
+
+
+    const index = categories.indexOf(category);
+    categories.splice(index , 1)
+    res.send(category)
 
 })
