6780667c5c7d1b96cecc129b593ad0cabbf6d270
Md Azaz <mdazaz9026@gmail.com>
2023-11-05 11:49:48 +0530

    setup all thing

--- project_work/Routes/categories.js
+++ project_work/Routes/categories.js
@@ -67,7 +67,7 @@ router.put('/api/categories/:id' , async (req , res) => {
 
 router.delete('/api/categories/:id' , async (req , res) => {
 
-    const category = await Category.findByIdAndRemove(req.params.id)
+    const category = await Category.findByIdAndDelete(req.params.id)
 
     if(!category) return res.status(404).send(' The Category with the given ID was not found. ')
 
